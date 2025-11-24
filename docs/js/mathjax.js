98b0bd765b729292fdb94cd607eb4fe54a90d6bb
whxhcj <haoxiangwu6@gmail.com>
2025-11-24 21:18:34 +0800

    xxx

--- docs/js/mathjax.js
+++ docs/js/mathjax.js
@@ -1,19 +1,20 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true
   },
   options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
+    ignoreHtmlClass: "^(?!arithmatex).*",  // 忽略非 arithmatex 的内容
+    // ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex" // 可以保留
   }
 };
 
-document$.subscribe(() => { 
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
